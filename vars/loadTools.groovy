6b17b6af91ede8af48a18e13969ffbbb87b149d4
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-09 14:57:53 +0700

    Remove comments from loadTools.groovy

--- vars/loadTools.groovy
+++ vars/loadTools.groovy
@@ -3,14 +3,10 @@ def call(String toolFile = 'tools.properties') {
         error "Tools properties file not found: ${toolFile}"
     }
 
-    // Read the properties file
     def toolsConfig = readProperties(file: toolFile)
 
-    // Iterate through the keys and values in the properties file
     toolsConfig.each { key, value ->
-        // Use the value to get the tool home
         def toolHome = tool(value.trim())
         env["${key.trim().toUpperCase()}_HOME"] = toolHome
-        echo "Loaded tool: ${key.trim()} = ${value.trim()}"
     }
 }
