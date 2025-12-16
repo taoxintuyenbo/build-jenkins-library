c15185fb1335ff56c3b5af8b0c9e9cb753639bde
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-16 22:20:52 +0700

    Create pushArtifactJava.groovy

---
+++ vars/pushArtifactJava.groovy
@@ -0,0 +1,19 @@
+def call() {
+  stage('Push artifact to Nexus Repo'){
+    if(env.BRANCH_NAME == 'releases'){
+        withCredentials([string(credentialsId: "${NEXUS_CRED}", variable: 'NEXUS_ACC')]) {
+          sh """curl -v -u ${NEXUS_ACC} --upload-file ${ARTIFACT_PATH} \
+            http://${NEXUS_URL}/repository/${NEXUS_RELEASES_REPO}/${NEXUS_GROUP}/${NEXUS_ARTIFACT_ID}/${env.BRANCH_NAME}/${env.VERSION}-${env.BRANCH_NAME}.jar
+          """
+        }
+    }
+    if(branch.startsWith('uat')){
+        def GIT_HASH = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+        withCredentials([string(credentialsId: "${NEXUS_CRED}", variable: 'NEXUS_ACC')]) {
+          sh """curl -v -u ${NEXUS_ACC} --upload-file ${ARTIFACT_PATH} \
+            http://${NEXUS_URL}/repository/${NEXUS_RELEASES_REPO}/${NEXUS_GROUP}/${NEXUS_ARTIFACT_ID}/${env.BRANCH_NAME}/${env.VERSION}-${env.BRANCH_NAME}-${GIT_HASH}.jar
+          """
+        }
+    }
+  }
+}
