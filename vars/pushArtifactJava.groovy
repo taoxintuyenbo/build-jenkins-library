28fb1c75451b108c9991574f4148f7e48e2f313d
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-18 13:48:19 +0700

    Update pushArtifactJava.groovy

--- vars/pushArtifactJava.groovy
+++ vars/pushArtifactJava.groovy
@@ -1,16 +1,16 @@
 def call() {
   stage('Push artifact to Nexus Repo'){
     if(env.BRANCH_NAME == 'releases'){
-        withCredentials([string(credentialsId: "${NEXUS_CRED}", variable: 'NEXUS_ACC')]) {
-          sh """curl -v -u ${NEXUS_ACC} --upload-file ${ARTIFACT_PATH} \
+        withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
+          sh """curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${ARTIFACT_PATH} \
             http://${NEXUS_URL}/repository/${NEXUS_RELEASES_REPO}/${NEXUS_GROUP}/${NEXUS_ARTIFACT_ID}/${env.BRANCH_NAME}/${env.VERSION}-${env.BRANCH_NAME}.jar
           """
         }
     }
     if(env.BRANCH_NAME.startsWith('uat')){
         def GIT_HASH = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
-        withCredentials([string(credentialsId: "${NEXUS_CRED}", variable: 'NEXUS_ACC')]) {
-          sh """curl -v -u ${NEXUS_ACC} --upload-file ${ARTIFACT_PATH} \
+        withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
+          sh """curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${ARTIFACT_PATH} \
             http://${NEXUS_URL}/repository/${NEXUS_RELEASES_REPO}/${NEXUS_GROUP}/${NEXUS_ARTIFACT_ID}/${env.BRANCH_NAME}/${env.VERSION}-${env.BRANCH_NAME}-${GIT_HASH}.jar
           """
         }
