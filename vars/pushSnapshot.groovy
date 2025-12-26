bb557cad66f0bdbe5fd2cf6110520c0d1eba3f93
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-26 08:43:52 +0700

    Update pushSnapshot.groovy

--- vars/pushSnapshot.groovy
+++ vars/pushSnapshot.groovy
@@ -1,9 +1,7 @@
 def call(){
   stage('Push Snapshot'){
      withCredentials([usernamePassword(credentialsId:  "${NEXUS_CRED}", usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
-        sh """
-          ${MAVEN_HOME}/bin/mvn clean deploy -Dmaven.test.skip=true -Dusername=${NEXUS_USER} -Dpassword=${NEXUS_PASS}
-        """
+        sh "${MAVEN_HOME}/bin/mvn clean deploy -Dmaven.test.skip=true -Dusername=${NEXUS_USER} -Dpassword=${NEXUS_PASS}"
     }
   }
 }
