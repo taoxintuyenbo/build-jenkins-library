5f8eb39800020fb77ae8673299fb715c307a94c5
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-26 14:23:41 +0700

    Update pushSnapshot.groovy

--- vars/pushSnapshot.groovy
+++ vars/pushSnapshot.groovy
@@ -1,5 +1,6 @@
 def call(){
   stage('Push Snapshot'){
+    def GIT_HASH = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
     withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
       sh """
         curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${ARTIFACT_PATH} \
