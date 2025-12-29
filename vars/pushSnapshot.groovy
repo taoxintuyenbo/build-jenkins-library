fe0b78eb6775c6cc0b62ccbc8e4917768527d8dd
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-29 15:18:34 +0700

    Update pushSnapshot.groovy

--- vars/pushSnapshot.groovy
+++ vars/pushSnapshot.groovy
@@ -4,7 +4,7 @@ def call(){
     withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
       sh """
         curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${ARTIFACT_PATH} \
-          http://${NEXUS_URL}/repository/maven-snapshots/${NEXUS_GROUP}/${NEXUS_ARTIFACT_ID}/${VERSION}/${NEXUS_ARTIFACT_ID}-${VERSION}-SNAPSHOTS.jar
+          http://${NEXUS_URL}/repository/maven-snapshots/${NEXUS_GROUP}/${NEXUS_ARTIFACT_ID}/${VERSION}-SNAPSHOTS/${NEXUS_ARTIFACT_ID}-${VERSION}-SNAPSHOTS.jar
       """
     }
   }
