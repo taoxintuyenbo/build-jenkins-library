0f652347bf635a27b53fe284abe961fbfa1ce99d
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-31 23:07:43 +0700

    Update buildDocker.groovy

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -3,7 +3,7 @@ def call() {
     if(env.BRANCH_NAME == 'releases'){
       sh "docker build -t ${NEXUS_URL_DOCKER}/${NEXUS_ARTIFACT_ID}:${VERSION}-${env.BRANCH_NAME} ."
     }
-    if(env.BRANCH_NAME.startWith('uat')){
+    if(env.BRANCH_NAME.startsWith('uat')){
       def GIT_HASH = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
       sh "docker build -t ${NEXUS_URL_DOCKER}/${NEXUS_ARTIFACT_ID}:${VERSION}-${env.BRANCH_NAME}-${GIT_HASH} ."
     }
