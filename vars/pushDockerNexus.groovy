a5b2ddcf29171c19e982c7b2b9e9d03492435eda
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-31 23:32:04 +0700

    Update pushDockerNexus.groovy

--- vars/pushDockerNexus.groovy
+++ vars/pushDockerNexus.groovy
@@ -2,15 +2,15 @@ def call(){
   stage('Push Docker Image to Nexus'){
     if(env.BRANCH_NAME == 'releases'){
       withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_ACC', passwordVariable: 'NEXUS_PASS')]){
-        sh "echo $NEXUS_PASS | docker login -u $NEXUS_ACC --password-stdin ${NEXUS_DOCKER_URL}"
-        sh "docker push ${NEXUS_DOCKER_URL}/docker-releases:${VERSION}-${env.BRANCH_NAME}"
+        sh "echo $NEXUS_PASS | docker login -u $NEXUS_ACC --password-stdin ${NEXUS_URL_DOCKER}"
+        sh "docker push ${NEXUS_URL_DOCKER}/docker-releases:${VERSION}-${env.BRANCH_NAME}"
       }
     }
     if(env.BRANCH_NAME.startsWith('uat')){
       def GIT_HASH = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
       withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_ACC', passwordVariable: 'NEXUS_PASS')]){
-        sh "echo $NEXUS_PASS | docker login -u $NEXUS_ACC --password-stdin ${NEXUS_DOCKER_URL}"
-        sh "docker push ${NEXUS_DOCKER_URL}/docker-releases:${VERSION}-${env.BRANCH_NAME}-${GIT_HASH}"
+        sh "echo $NEXUS_PASS | docker login -u $NEXUS_ACC --password-stdin ${NEXUS_URL_DOCKER}"
+        sh "docker push ${NEXUS_URL_DOCKER}/docker-releases:${VERSION}-${env.BRANCH_NAME}-${GIT_HASH}"
       }
     }
   }
