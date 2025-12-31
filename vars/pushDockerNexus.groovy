c10625598a50db415b6314bbc16b56ca0bf17db8
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2025-12-31 23:07:21 +0700

    Create pushDockerNexus.groovy

---
+++ vars/pushDockerNexus.groovy
@@ -0,0 +1,17 @@
+def call(){
+  stage('Push Docker Image to Nexus'){
+    if(env.BRANCH_NAME == 'releases'){
+      withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_ACC', passwordVariable: 'NEXUS_PASS')]){
+        sh "echo $NEXUS_PASS | docker login -u $NEXUS_ACC --password-stdin ${NEXUS_DOCKER_URL}"
+        sh "docker push ${NEXUS_DOCKER_URL}/${NEXUS_ARTIFACT_ID}:${VERSION}-${env.BRANCH_NAME}"
+      }
+    }
+    if(env.BRANCH_NAME.startsWith('uat')){
+      def GIT_HASH = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+      withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_ACC', passwordVariable: 'NEXUS_PASS')]){
+        sh "echo $NEXUS_PASS | docker login -u $NEXUS_ACC --password-stdin ${NEXUS_DOCKER_URL}"
+        sh "docker push ${NEXUS_DOCKER_URL}/${NEXUS_ARTIFACT_ID}:${VERSION}-${env.BRANCH_NAME}-${GIT_HASH}"
+      }
+    }
+  }
+}
