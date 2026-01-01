227a5f9d576e37b754a3247441f6b861059f7a66
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2026-01-01 16:22:21 +0700

    Change branch name check from 'releases' to 'release'

--- vars/pushArtifactJava.groovy
+++ vars/pushArtifactJava.groovy
@@ -1,6 +1,6 @@
 def call() {
   stage('Push artifact to Nexus Repo'){
-    if(env.BRANCH_NAME == 'releases'){
+    if(env.BRANCH_NAME == 'release'){
         withCredentials([usernamePassword(credentialsId: "${NEXUS_CRED}", usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
           sh """curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${ARTIFACT_PATH} \
             http://${NEXUS_URL}/repository/${NEXUS_RELEASES_REPO}/${NEXUS_GROUP}/${NEXUS_ARTIFACT_ID}/${VERSION}/${NEXUS_ARTIFACT_ID}-${VERSION}-${env.BRANCH_NAME}.jar
